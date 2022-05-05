80c4c0d1cd0e2d1209deb99a791248386aaaeec2
Jorge Armando Medina Rosas <jmedina.externo@coopeuch.cl>
2022-05-05 10:51:47 -0500

    Initial Import

---
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TailSpin.SpaceGame.Web
+{
+    public class LeaderboardFunctionClient : ILeaderboardServiceClient
+    {
+        private string _functionUrl;
+
+        public LeaderboardFunctionClient(string functionUrl)
+        {
+            this._functionUrl = functionUrl;
+        }
+
+        async public Task<LeaderboardResponse> GetLeaderboard(int page, int pageSize, string mode, string region)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string json = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
+                return JsonSerializer.Deserialize<LeaderboardResponse>(json);
+            }
+        }
+    }
+}
